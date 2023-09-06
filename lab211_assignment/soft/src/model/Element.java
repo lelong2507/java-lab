6e455895af96f3e58b597c0ef3ff58fe363d17dc
Long <longle2507@MacBook-Pro-cua-Long.local>
2023-09-06 11:09:00 +0700

    Tuesday commit

---
+++ lab211_assignment/soft/src/model/Element.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Element {
+    private int sizeArray;
+    private int[] arr;
+
+    public int getSizeArray() {
+        return sizeArray;
+    }
+
+    public void setSizeArray(int sizeArray) {
+        this.sizeArray = sizeArray;
+    }
+
+    public int[] getArr() {
+        return arr;
+    }
+
+    public void setArr(int[] arr) {
+        this.arr = arr;
+    }
+}
