a871e67f86924e46dcf77ca28f2fcd9f9e18a58e
Long Le <102498053+lelong2507@users.noreply.github.com>
2023-11-04 15:40:55 +0700

    Zip&UnZipFile "2023/11/04"

---
+++ P0079/src/main.java
@@ -0,0 +1,17 @@
+
+import controller.FileManager;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author longle2507
+ */
+public class main {
+    public static void main(String[] args) {
+        new FileManager().run();
+    }
+}
